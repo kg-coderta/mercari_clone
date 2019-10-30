3efb807ae6a63dcd01636ab3c73305f03680a034
YusukePro61 <learning630@gmail.com>
2019-10-30 17:52:23 +0900

    user_edit-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'mypages/index'
   get 'mypages/destroy'
+  get 'mypages/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   devise_for :users, controllers: {
