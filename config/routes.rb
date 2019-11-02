9e732fce4575920d2c0fde47c949551a834b1365
kg <ke123iji@gmail.com>
2019-11-02 15:45:53 +0900

    setting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :mypages, only: [:index, :destroy, :edit]
+  resources :mypages, only: [:index, :destroy, :edit] 
+  resources :addresses, only:[:new, :edit]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   devise_for :users, controllers: {
