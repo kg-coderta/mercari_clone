614063d80efaa16892a03517af20b60201dfa5f6
LOMOKIYO <am6966@i.softbank.jp>
2019-11-01 18:43:26 -0700

    first-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'items#index'
+  root 'mypages#card'
   resources :mypages, only: [:index, :destroy, :edit]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
