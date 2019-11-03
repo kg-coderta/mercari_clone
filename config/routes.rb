b306022b8522a00057716e0599acc0ed6cdaf7db
Yuma Tanaka <yumat9637@gmail.com>
2019-11-03 14:23:23 +0900

    create items

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
+  resources :items, only: [:index, :show]
   resources :mypages, only: [:index, :destroy, :edit]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
