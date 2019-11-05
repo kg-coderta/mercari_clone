8c7561e430d2d29473c61c9cfa2cc7c4e10d8e0c
LOMOKIYO <am6966@i.softbank.jp>
2019-11-05 18:59:56 +0900

    mer-buypage-end

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'items#buy'
+  root 'items#index'
   resources :items, only: [:index, :show]
   resources :mypages, only: [:index, :destroy, :edit]
   resources :addresses, only:[:new, :edit,:show]
