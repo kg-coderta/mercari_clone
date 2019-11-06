291fc1ea0bfa3ac9ec8f426b66f4a3185172d038
Yuma Tanaka <yumat9637@gmail.com>
2019-11-06 11:54:24 +0900

    view修正,　現状できる範囲のリンク付け

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+
+  def create
+    item = Item.find(params[:item_id])
+    @comment = item.comments.new(comment_params)
+    if @comment.save
+      redirect_to item_path(params[:item_id])
+    else
+      redirect_to item_path(params[:item_id])
+    end
+  end
+
+  def destroy
+    item = Item.find(params[:item_id])
+    @comment = item.comments.find(params[:id])
+    if @comment.user_id == current_user.id
+      @comment.destroy
+    else
+      redirect_to item_path(params[:item_id])
+    end
+    redirect_to item_path(params[:item_id])
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id)
+  end
+end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,24 @@
 class ItemsController < ApplicationController
+  # before_action :set
+
   def index
+    @item = Item.all
   end
 
   def show
+    @item = Item.includes(:photos).find(params[:id])
+    @photos = Photo.where(item_id: @item.id)
+    @saler = User.find(@item.saler_id)
+    @saler_items = Item.where(saler_id: @saler.id).limit(6).order('id DESC')
 
+    @comments = @item.comments.includes(:user)
+    @comment = Comment.new
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :state, :size, :method, :carriage, :region, :date, :price).merge(saler_id: current_user.id)
   end
 end
