81daea88df0e6d973b8d111bcf343e802cbc6113
kg <ke123iji@gmail.com>
2019-11-07 18:30:33 +0900

    add s3 settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,12 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock "~> 3.11.2"
 
+set :default_env, {
+  rbenv_root: "/usr/local/bin/rbenv",
+  path: "/usr/local/bin/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 # Capistranoのログの表示に利用する
 set :application, 'mercari_clone'
 
@@ -11,6 +17,7 @@ set :repo_url,  'git@github.com:kg-coderta/mercari_clone.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, %w{ config/secrets.yml }
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
@@ -33,6 +40,18 @@ namespace :deploy do
     invoke 'unicorn:stop'
     invoke 'unicorn:start'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
 
 # Default branch is :master
