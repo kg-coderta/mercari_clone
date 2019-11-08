0626fdfc7644ff9b77c2828f5d466a183421a2f6
YusukePro61 <learning630@gmail.com>
2019-11-08 21:03:13 +0900

    use_db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,28 +16,37 @@
 #               )
 # end
 
-Comment.seed do |i|
-  i.id = 1
-  i.text = 'kokjojojojoijijiminnnjnjn'
-  i.item_id = 1
-  i.user_id = 201
-end
-Comment.seed do |i|
-  i.id = 2
-  i.text = 'fsalgkoskagomsofgmogomo'
-  i.item_id = 1
-  i.user_id = 201
-end
+# Comment.seed do |i|
+#   i.id = 1
+#   i.text = 'kokjojojojoijijiminnnjnjn'
+#   i.item_id = 1
+#   i.user_id = 201
+# end
+# Comment.seed do |i|
+#   i.id = 2
+#   i.text = 'fsalgkoskagomsofgmogomo'
+#   i.item_id = 1
+#   i.user_id = 201
+# end
+
+
 
 # 2.times do |n|
 #   Item.create!(name: "布団",
-#                description: "あったかいよ、ともマン",
-#                state: "未使用",
-#                method: "出品者負担",
-#                region: "大阪府",
-#                date: "1~2日で発送",
-#                price: 5000,
-#                saler_id: 4,
-#                buyer_id: 10,
+#               description: "あったかいよ、ともマン",
+#               state: "未使用",
+#               method: "楽々メルカリ便",
+#               region: "大阪府",
+#               date: "1~2日で発送",
+#               price: 5000,
+#               saler_id: 4,
+#               buyer_id: 5,
+#               carriage: "出品者負担"
 #               )
 # end
+
+Photo.seed do |i|
+  i.id = 1
+  i.image = 'https://www.asahicom.jp/articles/images/AS20180806003279_commL.jpg'
+  i.item_id = 4
+end
