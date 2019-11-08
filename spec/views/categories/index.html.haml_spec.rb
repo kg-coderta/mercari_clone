a6174e5e31a72fb45a40e1a870072c0ce6f5bd1d
Yuma Tanaka <yumat9637@gmail.com>
2019-11-08 20:04:35 +0900

    テストファイル

--- spec/views/categories/index.html.haml_spec.rb
+++ spec/views/categories/index.html.haml_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
+RSpec.describe "categories/index.html.haml", type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
-
 end
