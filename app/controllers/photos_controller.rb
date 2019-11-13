06da6800f4c0a8ed62041af5e11af45a61ddcf5e
LOMOKIYO <am6966@i.softbank.jp>
2019-11-13 19:53:38 +0900

    fix8

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,4 @@
 class PhotosController < ApplicationController
-  # params.require(:item).permit(:name,photos_attributes:[:image])
-  # 5.times{@item.photos.build}
-  # skip_before_action :verify_authenticity_token
 
   def new
   end
