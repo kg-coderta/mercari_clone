52ec2f71f3f8b03e18a07f4bbb1befbc8f3f4c8b
YusukePro61 <learning630@gmail.com>
2019-11-13 13:49:00 +0900

    remove_route

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :addresses, only:[:new, :edit, :show]
   get "addresses" => "addresses#phone"
   root 'items#index'
-  delete 'items/:id' => 'items#destroy'
+  # delete 'items/:id' => 'items#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   devise_for :users, controllers: {
