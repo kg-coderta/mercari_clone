99aaf3b436502f7b18a8bd04d1af096f91966168
kg <ke123iji@gmail.com>
2019-11-14 19:00:05 +0900

    minor change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[facebook google_oauth2]
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable,
+          :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
   # omniauthのコールバック時に呼ばれるメソッド
   def self.from_omniauth(auth)
@@ -19,12 +19,11 @@ class User < ApplicationRecord
   has_many :saling_items, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name: "Item"
   has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Item"
   has_one :adress, dependent: :destroy
-  has_one :profile, dependent: :destroy
   has_one :card, dependent: :destroy
-  has_many :comments, dependent: :destroy
-
-  validates :nickname, presence: true
-  validates :nickname, length: { maximum: 20 }
+  has_many :comments, dependent: :destroy 
+  validates :nickname, presence: true, length: { maximum: 20 }
+  validates :email, uniqueness: true, format: { with: /\A[a-zA-Z]+\z/,
+  message: "英文字のみが使用できます" }
   validates :password, length: { minimum: 7 }
-  validates :password_confirmation, presence: true, length: { minimum: 7 }
+  validates :password_confirmation, presence: true, length: { minimum: 7 } 
 end
