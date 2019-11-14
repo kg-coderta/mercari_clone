998cb4c06c95afac5d42cb8d3bc6169421cec2b4
kg <ke123iji@gmail.com>
2019-11-14 18:44:54 +0900

    add routing and views for signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,15 @@ Rails.application.routes.draw do
   resources :mypages, only: [:index, :destroy, :edit, :show]
   resources :addresses, only:[:new, :edit, :show]
   get "addresses" => "addresses#phone"
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4' 
+      get 'done' # 登録完了後のページ
+    end
+  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
