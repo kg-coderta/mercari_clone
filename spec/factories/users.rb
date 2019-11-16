10e0901dc18d0d98403b258356bd3942e01fad59
YusukePro61 <learning630@gmail.com>
2019-11-16 20:29:06 +0900

    update_FactoryBot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,17 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              {"abe"}
+    nickname              {"abechan"}
     email                 {"kkk@gmail.com"}
-    password              {"0000000"}
-    password_confirmation {"0000000"}
+    password              {"abc0000"}
+    password_confirmation {"abc0000"}
+    first_name            {"abe"}
+    last_name             {"hiroshi"}
+    first_name_kana       {"アベ"}
+    last_name_kana        {"ヒロシ"}
+    birth_year_id         { 1964 }
+    birth_month_id        { 6 }
+    birth_day_id          { 22 }
   end
 
 end
